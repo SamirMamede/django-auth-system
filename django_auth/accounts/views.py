f825d6767310468b6507b66fe22fe86f03162c7c
SamirMamede <anuarsamir@gmail.com>
2024-10-10 22:51:48 -0400

    Protected private pages with authentication in accounts app

--- django_auth/accounts/views.py
+++ django_auth/accounts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def private_page_one(request):
     return render(request, 'accounts/private_page_one.html')
 
+@login_required
 def private_page_two(request):
     return render(request, 'accounts/private_page_two.html')
