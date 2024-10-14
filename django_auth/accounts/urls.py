8a8953a12bf9211ec4cf9e5d76f6eac396592064
SamirMamede <anuarsamir@gmail.com>
2024-10-14 00:58:48 -0400

    Implement login functionality with error handling in accounts app

--- django_auth/accounts/urls.py
+++ django_auth/accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    path('login_user/', views.login_user, name='login_user'),
     path('private_page_one/', views.private_page_one, name='private_page_one'),
     path('private_page_two/', views.private_page_two, name='private_page_two'),
 ]
--- django_auth/accounts/views.py
+++ django_auth/accounts/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+from .forms import LoginUserForm
 
+def login_user(request):
+    error_message = ''
+
+    if request.method == 'POST':
+        form = LoginUserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('accounts:private_page_one')
+            else:
+                error_message = 'Invalid username or password. Try again.'
+        else:
+            error_message = 'Invalid username or password. Try again.'
+    else:
+        form = LoginUserForm()
+
+    form = LoginUserForm()
+    return render(request, 'accounts/login_user.html', {'form': form, 'error_message': error_message})
 @login_required
 def private_page_one(request):
     return render(request, 'accounts/private_page_one.html')
