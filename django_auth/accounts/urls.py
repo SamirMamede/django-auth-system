ea6da32128ffcc7f32c72482d7a4ff29b5111c53
SamirMamede <anuarsamir@gmail.com>
2024-10-17 22:58:02 -0400

    Refactor login functionality to use Django's built-in authentication views in accounts app

--- django_auth/accounts/urls.py
+++ django_auth/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login_user/', views.login_user, name='login_user'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout_user/', views.logout_user, name='logout_user'),
     path('private_page_one/', views.private_page_one, name='private_page_one'),
     path('private_page_two/', views.private_page_two, name='private_page_two'),
--- django_auth/accounts/views.py
+++ django_auth/accounts/views.py
@@ -1,37 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import logout
 from django.shortcuts import redirect
 from .forms import LoginUserForm
-
-def login_user(request):
-    error_message = ''
-    next_url = request.POST.get('next') or request.GET.get('next') or ''
-
-    if request.method == 'POST':
-        form = LoginUserForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-
-            if user is not None:
-                login(request, user)
-                redirect_url = next_url or 'accounts:private_page_one'
-                return redirect(redirect_url)
-            else:
-                error_message = 'Invalid username or password. Try again.'
-        else:
-            error_message = 'Invalid username or password. Try again.'
-    else:
-        form = LoginUserForm()
-
-    form = LoginUserForm()
-    return render(request, 'accounts/login_user.html', {'form': form, 'error_message': error_message, 'next_url': next_url})
-
-def logout_user(request):
-    logout(request)
-    return redirect('accounts:login_user')
 
 @login_required
 def private_page_one(request):
