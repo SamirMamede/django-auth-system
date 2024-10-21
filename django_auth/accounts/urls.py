7fa65b5e39cceb84c2b76370afd51726a73067cd
SamirMamede <anuarsamir@gmail.com>
2024-10-20 23:48:03 -0400

    Added password reset link and URL path

--- django_auth/accounts/urls.py
+++ django_auth/accounts/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('private_page_two/', views.private_page_two, name='private_page_two'),
     path('password_change/', auth_views.PasswordChangeView.as_view(success_url='/password_change_done/'), name='password_change'),
     path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(),name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
 ]
