1f24fe043151c9b48cfc14dcbad3cff23711bd63
SamirMamede <anuarsamir@gmail.com>
2024-10-24 19:38:29 -0400

    Add password reset confirmation and completion views, included links for users to log in after resetting their password.

--- django_auth/accounts/urls.py
+++ django_auth/accounts/urls.py
@@ -5,28 +5,54 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', 
+    path(
+        'login/', 
          auth_views.LoginView.as_view(), 
-         name='login'),
-    path('logout/', 
+         name='login'
+         ),
+    path(
+        'logout/', 
          auth_views.LogoutView.as_view(), 
-         name='logout'),
-    path('private_page_one/', 
+         name='logout'
+         ),
+    path(
+        'private_page_one/', 
          views.private_page_one, 
-         name='private_page_one'),
-    path('private_page_two/', 
+         name='private_page_one'
+         ),
+    path(
+        'private_page_two/', 
          views.private_page_two, 
-         name='private_page_two'),
-    path('password_change/', 
-         auth_views.PasswordChangeView.as_view(success_url='/password_change_done/'), 
-         name='password_change'),
-    path('password_change_done/', 
+         name='private_page_two'
+         ),
+    path(
+        'password_change/', 
+         auth_views.PasswordChangeView.as_view(success_url='/accounts/password_change_done'), 
+         name='password_change'
+         ),
+    path(
+        'password_change_done/', 
          auth_views.PasswordChangeDoneView.as_view(),
-         name='password_change_done'),
-    path('password_reset/', 
-         auth_views.PasswordResetView.as_view(success_url='/password_reset_done/'), 
-         name='password_reset'),
-    path('password_reset/done/', 
+         name='password_change_done'
+         ),
+    path(
+        'password_reset/', 
+         auth_views.PasswordResetView.as_view(success_url='/accounts/password_reset_done'), 
+         name='password_reset'
+         ),
+    path(
+        'password_reset_done/', 
          auth_views.PasswordResetDoneView.as_view(), 
-         name='password_reset_done'),
+         name='password_reset_done'
+         ),
+    path(
+        'password_reset_confirm/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(success_url='/accounts/password_reset_complete'), 
+         name='password_reset_confirm'
+         ),
+    path(
+        'password_reset_complete/', 
+         auth_views.PasswordResetCompleteView.as_view(), 
+         name='password_reset_complete'
+         ),
 ]
